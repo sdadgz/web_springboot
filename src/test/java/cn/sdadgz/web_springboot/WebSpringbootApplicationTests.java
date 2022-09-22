31a29e753d2e6f3794fa6a9af2f0fe1b3d08043f
sdadgz <pengchengma5@gmail.com>
2022-09-22 10:58:35 +0800

    。

--- src/test/java/cn/sdadgz/web_springboot/WebSpringbootApplicationTests.java
+++ src/test/java/cn/sdadgz/web_springboot/WebSpringbootApplicationTests.java
@@ -19,6 +19,8 @@ import javax.imageio.stream.FileImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -35,6 +37,12 @@ class WebSpringbootApplicationTests {
     void contextLoads() throws IOException, NoSuchAlgorithmException {
         String path = "D:\\下载\\不用思考，删这个文件夹\\258969461563168455685717945467846336308illust_91452046_20210724_134755.jpg";
         String reducePath = "D:\\下载\\不用思考，删这个文件夹\\reduce.jpg";
+
+        int[] arr = {1,55,315,4651,21,44,2213,0,-5};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
 
 //        Thumbnails.of(path).scale(1).outputQuality(.8).toFile(path + ".jpg");
     }
