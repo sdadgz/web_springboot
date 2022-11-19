25124772c625a08026b71b67aa26b354c94eac91
sdadgz <pengchengma5@gmail.com>
2022-11-19 11:11:56 +0800

    19

--- src/main/java/cn/sdadgz/web_springboot/WebSpringbootApplication.java
+++ src/main/java/cn/sdadgz/web_springboot/WebSpringbootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebSpringbootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebSpringbootApplication.class, args);
     }
-
 }
