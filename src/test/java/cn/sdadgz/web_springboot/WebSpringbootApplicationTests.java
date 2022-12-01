59486f6591e6892bf0689aadc49ad972a19b0027
sdadgz <pengchengma5@gmail.com>
2022-12-01 17:44:39 +0800

    1

--- src/test/java/cn/sdadgz/web_springboot/WebSpringbootApplicationTests.java
+++ src/test/java/cn/sdadgz/web_springboot/WebSpringbootApplicationTests.java
@@ -1,13 +1,9 @@
 package cn.sdadgz.web_springboot;
 
-import cn.sdadgz.web_springboot.entity.File;
-import cn.sdadgz.web_springboot.entity.Img;
-import cn.sdadgz.web_springboot.entity.User;
 import cn.sdadgz.web_springboot.mapper.BlogMapper;
 import cn.sdadgz.web_springboot.mapper.FileMapper;
 import cn.sdadgz.web_springboot.mapper.ImgMapper;
 import cn.sdadgz.web_springboot.mapper.UserMapper;
-import cn.sdadgz.web_springboot.scheduled.ImgScheduled;
 import cn.sdadgz.web_springboot.service.IImgService;
 import cn.sdadgz.web_springboot.utils.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 @SpringBootTest
 @Slf4j
