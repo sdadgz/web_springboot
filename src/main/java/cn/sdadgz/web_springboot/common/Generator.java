c05bae812235c051cd28358b380fa807c9feb7ab
sdadgz <pengchengma5@gmail.com>
2022-12-16 17:35:58 +0800

    构造器

--- src/main/java/cn/sdadgz/web_springboot/common/Generator.java
+++ src/main/java/cn/sdadgz/web_springboot/common/Generator.java
@@ -16,7 +16,7 @@ public class Generator {
 //        String localPath = "D:/CODE/";
 
         // 表名
-        String tableName = "ip_ban";
+        String tableName = "log";
 
         FastAutoGenerator.create(
                         "jdbc:mysql://sdadgz.cn:3307/website",
