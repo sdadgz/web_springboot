bc766c33e0c91c43b556a220c49233c6f0071cd1
sdadgz <pengchengma5@gmail.com>
2023-03-21 20:00:08 +0800

    一键部署

---
+++ upload.bat
@@ -0,0 +1,3 @@
+chip 65001
+mvn package && scp D:\工具\编程\idea\java工作区\web_springboot\target\web_springboot-0.0.1-SNAPSHOT.jar 02:/www/www.sdadgz.cn/springboot/web_springboot-0.0.1-SNAPSHOT.jar
+ssh 02
